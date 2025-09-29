fe597bbb957e1e3870573ca634560404a3d046b4
nomayen31 <nomayen.ohin@gmail.com>
2025-09-29 21:29:26 +0600

    feature and talent section add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import Navbar from '../../Components/Navbar'
 import HeroSection from '../../Components/HeroSection'
+import Features from '../../Components/Features'
+import Talent from '../../Components/Talent'
 
 const Home = () => {
   return (
-    <div>
-      <HeroSection/>
-    </div>
+     <div className='bg-[#1A3A37] min-h-screen font-sans'>
+            {/* Navbar is typically here, omitted for brevity */}
+            <HeroSection/>
+            <Features/>
+            <Talent/>
+        </div>
   )
 }
 
